510310543dc99dd0670028eb18029405644e274f
Gregory Petukhov <lorien@lorien.name>
2017-11-01 18:57:34 +0300

    Add events to utility test

--- src/Utility.sol
+++ src/Utility.sol
@@ -1,9 +1,18 @@
 pragma solidity ^0.4.14;
 
 contract Utility {
-	function getLastBlockHash() returns (bytes32 _blockHash) {
+    event Block(uint256 number, bytes32 hash);
+
+	function getLastBlockHash() returns (bytes32) {
 	    uint256 lastBlockNumber = block.number - 1;
-	    bytes32 lastBlockHash = bytes32(block.blockhash(lastBlockNumber));
+	    bytes32 lastBlockHash = block.blockhash(lastBlockNumber);
+
+        Block(0, lastBlockHash);
+        Block(block.number - 1, block.blockhash(block.number - 1));
+        Block(block.number - 2 , block.blockhash(block.number - 2));
+        Block(block.number - 3, block.blockhash(block.number - 3));
+        Block(block.number - 4, block.blockhash(block.number - 4));
+
 	    return lastBlockHash;
 	}
 }
--- test/utility.js
+++ test/utility.js
@@ -15,12 +15,23 @@ contract('Utility [all features]', function(accounts) {
     });
 
     it('getLastBlockHash', async () => {
+
+        let bnum = await web3.eth.blockNumber;
+        for(let idx=0; idx<4; idx++) {
+            bnum -= 1;
+            let hash = web3.eth.getBlock(bnum).hash;
+            console.log('Block [' + bnum + ']: ' + hash); 
+        }
         let blockNumber = await web3.eth.blockNumber;
         await utilityContract.getLastBlockHash.call();
         // check that call to method does not make testrpc mine new block
         assert.equal(blockNumber, web3.eth.blockNumber);
 
-        let blockHash = await web3.eth.getBlock(blockNumber - 1).hash;
+        let res = await utilityContract.getLastBlockHash();
+        res.logs.forEach((item) => {
+            console.log(item.args);
+        });
+        let blockHash = await web3.eth.getBlock(blockNumber).hash;
         blockHash.should.be.equal(
             await utilityContract.getLastBlockHash.call()
         );
