f29aba0f68b1d18aef257ad950b8a4381b5fd10e
Gregory Petukhov <lorien@lorien.name>
2017-11-02 22:28:35 +0300

    Use owner address in correct format (valid checksum)

--- src/util/KeysManagerProxy.sol
+++ src/util/KeysManagerProxy.sol
@@ -2,7 +2,7 @@ import './../KeysManager.sol';
 
 contract KeysManagerProxy is KeysManager {
     function KeysManagerProxy() public KeysManager() {
-        owner = 0x338a7867a35367d120011b2da1d8e2a8a60b9bc0;
+        owner = 0x338a7867A35367D120011B2DA1D8E2a8A60B9bC0;
     }
 
     function getInitialKeysIssued() public view returns (int8) {
--- test/data.js
+++ test/data.js
@@ -1,7 +1,7 @@
 let big = require('./util/bigNum.js').big;
 
 const ETHER = big(10).toPower(18);
-const SYSTEM_OWNER_ADDRESS = '0x338a7867a35367d120011b2da1d8e2a8a60b9bc0'
+const SYSTEM_OWNER_ADDRESS = '0x338a7867A35367D120011B2DA1D8E2a8A60B9bC0'
 const INITIAL_KEYS_LIMIT = 25;
 
 module.exports = {
