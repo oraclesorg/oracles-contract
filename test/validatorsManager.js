8c51a6de707a68a069b7559f254e38f18875a3c9
Gregory Petukhov <lorien@lorien.name>
2017-11-04 16:25:23 +0300

    Add tests for getValidator* methods

--- test/validatorsManager.js
+++ test/validatorsManager.js
@@ -23,8 +23,8 @@ contract('validatorsManager [all features]', function(accounts) {
         voting: addressFromNumber(6),
     }
     let data1 = {
-        zip: 644081,
-        licenseExpiredAt: 1893456000, // 2030 year
+        zip: big(644081),
+        licenseExpiredAt: big(1893456000), // 2030 year
         licenseID: 'license-1',
         fullName: 'Ivan Ivanov',
         streetName: 'Elm Street',
@@ -156,4 +156,40 @@ contract('validatorsManager [all features]', function(accounts) {
         );
 
     });
+
+    it.only('getValidator* methods', async () => {
+        await validatorsManager.addInitialKey(accounts[0], {from: systemOwner});
+        await validatorsManager.addValidator(
+            keys1.mining,
+            data1.zip,
+            data1.licenseExpiredAt,
+            data1.licenseID,
+            data1.fullName,
+            data1.streetName,
+            data1.state,
+            {from: accounts[0]}
+        );
+        data1.zip.should.be.bignumber.equal(
+            await validatorsManager.getValidatorZip.call(keys1.mining)
+        );
+        data1.licenseExpiredAt.should.be.bignumber.equal(
+            await validatorsManager.getValidatorLicenseExpiredAt.call(keys1.mining)
+        );
+        data1.licenseID.should.be.equal(
+            await validatorsManager.getValidatorLicenseID.call(keys1.mining)
+        );
+        data1.fullName.should.be.equal(
+            await validatorsManager.getValidatorFullName.call(keys1.mining)
+        );
+        data1.streetName.should.be.equal(
+            await validatorsManager.getValidatorStreetName.call(keys1.mining)
+        );
+        data1.state.should.be.equal(
+            await validatorsManager.getValidatorState.call(keys1.mining)
+        );
+        big(0).should.be.bignumber.equal(
+            await validatorsManager.getValidatorDisablingDate.call(keys1.mining)
+        );
+    });
+
 });
